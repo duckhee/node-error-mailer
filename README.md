bb03be7e544232d705be1e8c70f23e33c8c29076
Jeroen Peerbolte <jeroenpeerbolte@gmail.com>
2016-03-10 14:38:32 +0100

    completes first usable version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # node-error-mailer
 
 simple module that allows you to send errors to an e-mail address
+
+## TODO 
+
+- Add throttle to max errors per time period
--- index.js
+++ index.js
@@ -3,16 +3,14 @@ var nodemailer = require('nodemailer');
 exports = module.exports = {};
 
 var transporter;
+var smtpConfig;
+var mailOptions;
 
-/*
-    USERSTORIES
-    
-    - I want to receive max 1 e-mail every 5 minutes 
-    -
-    
-*/
-
-
+/******************************************************
+ * 
+ *          PRIVATE FUNCTIONS
+ * 
+ ******************************************************/
 
 //  uses nodemailer to setup 
 //  example object:
@@ -24,23 +22,70 @@ var transporter;
 //          pass: 'password'
 //      }
 //  }
-function setupSmtpTransporter(smtpConfig){
+function _setupSmtpTransporter(_smtpConfig) {
+    smtpConfig = _smtpConfig;
     transporter = nodemailer.createTransport(smtpConfig);
 }
 
 // example mail options
 // var mailOptions = {
-//     from: fromName +' ✔ <'+ fromEmail +'>', // sender address
-//     to: to.join(), // list of receivers
+//     fromName: error log name 
+//     fromEmail: user@yourdomain.com
+//     to: String or array 
 // };
-function setMailOptions(options){
-    
+function _setMailOptions(options) {
+
+    if (!options.fromEmail) throw new Error('no from e-mail specified');
+    if (!options.toEmail) throw new Error('no toEmail specified');
+
+    var fromName = options.fromName || "Node Error Mailer";
+    var to = [].concat(options.toEmail);
+    console.log(to);
+
+    mailOptions = {
+        from: fromName + ' <' + options.fromEmail + '>',
+        to: to.join(","),
+        subject: 'Error Notification for ' + options.appName
+    };
+
 }
 
-function send(error){
-    
+/******************************************************
+ * 
+ *          PUBLIC FUNCTIONS
+ * 
+ ******************************************************/
+
+function send(errorMessage, callback) {
+
+    if (!mailOptions) callback('no mailOptions set');
+    if (!transporter) callabck('no transporter set');
+
+    callback = callback || function () { };
+    console.log('sending error', mailOptions);
+
+    var options = {
+        from: mailOptions.from,
+        to: mailOptions.to,
+        subject: mailOptions.subject,
+        text: errorMessage,
+        html: errorMessage
+    }
+    // send mail with defined transport object
+    // transporter.sendMail(options, function (err, info) {
+
+    //     if (err) return callback(err);
+    //     return callback(err, info);
+
+    // });
+
+
 }
 
-function init(smtpConfig, mailOptions){
-    
+function init(options) {
+    _setupSmtpTransporter(options.smpt);
+    _setMailOptions(options);
 }
+
+exports.send = send;
+exports.init = init;
